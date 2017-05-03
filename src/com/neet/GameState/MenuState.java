d6857e07e7254d7259efd8c30bda4cbd5184fa60
Wilhelm <nightmare.v.94@gmail.com>
2017-05-03 10:41:22 +0500

    Initial update

---
+++ src/com/neet/GameState/MenuState.java
@@ -0,0 +1,123 @@
+package com.neet.GameState;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import com.neet.Audio.JukeBox;
+import com.neet.Entity.PlayerSave;
+import com.neet.Handlers.Keys;
+import com.neet.Main.GamePanel;
+
+public class MenuState extends GameState {
+	
+	private BufferedImage head;
+	
+	private int currentChoice = 0;
+	private String[] options = {
+		"Start",
+		"Quit"
+	};
+	
+	private Color titleColor;
+	private Font titleFont;
+	
+	private Font font;
+	private Font font2;
+	
+	public MenuState(GameStateManager gsm) {
+		
+		super(gsm);
+		
+		try {
+			
+			head = ImageIO.read(
+				getClass().getResourceAsStream("/HUD/Hud.gif")
+			).getSubimage(0, 12, 12, 11);
+			
+			titleColor = Color.WHITE;
+			titleFont = new Font("Times New Roman", Font.PLAIN, 28);
+			font = new Font("Arial", Font.PLAIN, 14);
+			font2 = new Font("Arial", Font.PLAIN, 10);
+			
+			JukeBox.load("/SFX/menuoption.mp3", "menuoption");
+			JukeBox.load("/SFX/menuselect.mp3", "menuselect");
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void init() {}
+	
+	public void update() {
+		
+		handleInput();
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		
+		g.setColor(titleColor);
+		g.setFont(titleFont);
+		g.drawString("SHITTY GAME BY WILHELM", 70, 90);
+		
+		g.setFont(font);
+		g.setColor(Color.WHITE);
+		g.drawString("Старт", 145, 165);
+		g.drawString("Выход", 145, 185);
+		
+		if(currentChoice == 0) g.drawImage(head, 125, 154, null);
+		else if(currentChoice == 1) g.drawImage(head, 125, 174, null);
+		
+		g.setFont(font2);
+		g.drawString("2015 Wilhelm.", 10, 232);
+		
+	}
+	
+	private void select() {
+		if(currentChoice == 0) {
+			JukeBox.play("menuselect");
+			PlayerSave.init();
+			gsm.setState(GameStateManager.LEVEL1ASTATE);
+		}
+		else if(currentChoice == 1) {
+			System.exit(0);
+		}
+	}
+	
+	public void handleInput() {
+		if(Keys.isPressed(Keys.ENTER)) select();
+		if(Keys.isPressed(Keys.UP)) {
+			if(currentChoice > 0) {
+				JukeBox.play("menuoption", 0);
+				currentChoice--;
+			}
+		}
+		if(Keys.isPressed(Keys.DOWN)) {
+			if(currentChoice < options.length - 1) {
+				JukeBox.play("menuoption", 0);
+				currentChoice++;
+			}
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
